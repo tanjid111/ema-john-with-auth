3c464b9201de654620016a6931c68cc14eb8df26
tanjid111 <tanjidhossain@gmail.com>
2022-04-09 17:17:55 -0700

    implemented login

---
+++ src/__auth_steps.js
@@ -0,0 +1,22 @@
+/* 
+1. create a new firebase project in console.firebase.google.com 
+2. npm install firebase
+3. create firebase.init.js and import getAuth to export auth
+4. Firebase settings > Authentication > Enable email and password auth
+5. Create Login, Signup component, setup route
+6. Attach form field handler and form submit handler
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword from react firebase hooks
+9. if user is created redirect to the expected page
+10. useSignInWithEmailAndPassword for sign in
+
+*/
+
+/* 
+Hosting steps
+1. npm install -g firebase-tools
+2. firebase login
+3. firebase init
+4. firebase deploy
+
+*/
---
+++ src/components/Login/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './Login.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
+const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // const [error, setError] = useState('');
+
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordBlur = e => {
+        setPassword(e.target.value)
+    }
+
+    const handleUserSignIn = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+                <form onSubmit={handleUserSignIn}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p>loading...</p>
+                    }
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>New to Ema-jon? <Link className='form-link' to='/signup'>Create New Account</Link> </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Login; <h2>This is login</h2>
